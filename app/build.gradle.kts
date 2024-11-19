72434108f81e9ce5bfad5f3dccb827d64d666cff
krazyjoy <huang.joy.0506@gmail.com>
2024-11-18 21:25:06 -0700

    add styling

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,6 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android)
     id("kotlin-kapt")
     id("com.google.dagger.hilt.android")
-    id("com.google.devtools.ksp")
-    id("com.google.gms.google-services")
 }
 
 android {
@@ -13,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "dev.sudhanshu.calender"
-        minSdk = 26
+        minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -54,7 +52,7 @@ android {
 }
 
 dependencies {
-    //implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
@@ -62,17 +60,17 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material)
-    //implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.material3)
+
     implementation(libs.androidx.work.runtime.ktx)
     implementation(libs.androidx.hilt.common)
-    implementation(libs.firebase.messaging.ktx)
     testImplementation(libs.junit)
     implementation(libs.accompanist.systemuicontroller)
     androidTestImplementation(libs.androidx.junit)
-    //androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
@@ -99,7 +97,7 @@ dependencies {
     implementation(libs.okhttp)
 
     // Kotlin Coroutines dependencies
-    implementation(libs.kotlinx.coroutines.android) 
+    implementation(libs.kotlinx.coroutines.android)
 
     // Hilt dependencies
     implementation(libs.hilt.android)
@@ -107,7 +105,7 @@ dependencies {
     implementation(libs.androidx.hilt.navigation.compose)
 
     // Lifecycle components
-    //implementation(libs.androidx.lifecycle.runtime.ktx.v282)
+    implementation(libs.androidx.lifecycle.runtime.ktx.v282)
 
     // View model
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
@@ -115,29 +113,7 @@ dependencies {
     // google font
     implementation(libs.androidx.ui.text.google.fonts)
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-    //Glide
-    implementation("com.github.bumptech.glide:glide:4.14.2")
-    //Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("de.hdodenhof:circleimageview:3.1.0")
-    implementation("com.google.android.gms:play-services-auth:21.0.0")
-    implementation("com.google.api-client:google-api-client-android:1.32.1")    {
-        exclude(group = "org.apache.httpcomponents", module = "httpclient")
-    }
-    implementation("com.google.apis:google-api-services-calendar:v3-rev411-1.25.0") {
-        exclude(group = "org.apache.httpcomponents", module = "httpclient")
-    }
-    implementation("org.nanohttpd:nanohttpd:2.3.1")
-
+    // room
     val room_version = "2.6.1"
 
     implementation("androidx.room:room-runtime:$room_version")
@@ -146,7 +122,4 @@ dependencies {
 
     // To use Kotlin annotation processing tool (kapt)
     kapt("androidx.room:room-compiler:$room_version")
-
-    implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
-    implementation("com.google.firebase:firebase-analytics")
 }
--- app/src/main/java/dev/sudhanshu/calender/presentation/view/AddTaskDialog.kt
+++ app/src/main/java/dev/sudhanshu/calender/presentation/view/AddTaskDialog.kt
@@ -1,47 +1,33 @@
 package dev.sudhanshu.calender.presentation.view
 
 
-import android.app.TimePickerDialog
-
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.LocalContext
-
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.window.Dialog
 import androidx.hilt.navigation.compose.hiltViewModel
-
+import dev.sudhanshu.calender.data.model.Task
 import dev.sudhanshu.calender.data.model.TaskModel
 import dev.sudhanshu.calender.domain.model.TaskRequest
 import dev.sudhanshu.calender.presentation.viewmodel.TaskViewModel
 
-import android.util.Log
-import android.widget.TimePicker
-
-import kotlinx.coroutines.Dispatchers
-
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
-import java.sql.Time
-
-import java.util.Calendar
-import java.util.TimeZone
-import java.text.SimpleDateFormat
-import java.util.Date
-
-private val job = Job()
-private val coroutineScope = CoroutineScope(Dispatchers.Main + job)
 
 @Composable
 fun AddTaskDialog(
@@ -55,9 +41,6 @@ fun AddTaskDialog(
 
     var title by remember { mutableStateOf("") }
     var description by remember { mutableStateOf("") }
-    val context = LocalContext.current
-    var startTime by remember { mutableStateOf(("")) }
-    var endTime by remember { mutableStateOf(("")) }
 
     Dialog(onDismissRequest = { onDismiss() }) {
         Card(
@@ -74,12 +57,6 @@ fun AddTaskDialog(
                     fontSize = 20.sp,
                     modifier = Modifier.padding(bottom = 16.dp),
                     color = MaterialTheme.colors.onBackground
-                )
-                Text(
-                    text="Start Time: $startTime"
-                )
-                Text(
-                    text="End Time: $endTime"
                 )
 
                 OutlinedTextField(
@@ -99,41 +76,7 @@ fun AddTaskDialog(
                         .padding(vertical = 8.dp),
                     label = { Text("Description") }
                 )
-                Button(onClick={
-                    val calendar = Calendar.getInstance()
-                    val timePickerDialog = TimePickerDialog(
-                        context,
-                        {
-                            _:TimePicker, hourOfDay: Int, minute: Int->
-                            startTime = String.format("%02d:%2d", hourOfDay, minute)
-                        },
-                        calendar.get(Calendar.HOUR_OF_DAY),
-                        calendar.get(Calendar.MINUTE),
-                        true
-                    )
-                    timePickerDialog.show()
 
-                }){
-                    Text(text="Pick start Time")
-                }
-
-                Button(onClick={
-                    val calendar = Calendar.getInstance()
-                    val timePickerDialog = TimePickerDialog(
-                        context,
-                        {
-                                _: TimePicker, hourOfDay: Int, minute: Int->
-                            endTime = String.format("%02d:%2d", hourOfDay, minute)
-                        },
-                        calendar.get(Calendar.HOUR_OF_DAY),
-                        calendar.get(Calendar.MINUTE),
-                        true
-                    )
-                    timePickerDialog.show()
-
-                }){
-                    Text(text="Pick end Time")
-                }
                 Row(
                     modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.End,
@@ -157,38 +100,6 @@ fun AddTaskDialog(
                             }, onError = {
                                 onDismiss()
                             })
-                            //addEvent(title, date)
-                            Log.d("add task", "date: $date")
-                            Log.d("add task", "startTime: $startTime")
-                            val startDateTime = convertToSimpleDate(date, startTime)
-                            val endDateTime = convertToSimpleDate(date,endTime)
-
-                            Log.d("add task", "date: $startDateTime")
-                            Log.d("add task", "startTime: $endDateTime")
-
-                            if (compareTimes(date, startTime, date, endTime)) {
-                                Log.d("add task", "endTime is greater than startTime")
-                            } else {
-                                Log.d("add task", "endTime is not greater than startTime")
-                                onDismiss()
-                            }
-                            val insertTask = InsertTask(context)
-                            insertTask.insertEventToGoogleCalendar(
-                                accessToken = GoogleSignInHelper.accesstoken ?: "",
-                                summary = title,
-                                startDateTime = startDateTime,
-                                endDateTime = endDateTime,
-                                onSuccess = { eventId ->
-                                    Log.d("CalendarIntegration", "Event inserted with ID: $eventId")
-                                    onSaveTask() // Update the UI or dismiss the dialog
-                                },
-                                onError = { errorCode ->
-                                    Log.e("CalendarIntegration", "Error inserting event: $errorCode")
-                                    onDismiss()
-                                }
-                            )
-                            Log.d("CalendarIntegration","Time : $date : $time")
-
                         }
                     ) {
                         Text(text = "Save")
@@ -197,19 +108,4 @@ fun AddTaskDialog(
             }
         }
     }
-}
-
-fun convertToSimpleDate(dateInput: String, timeInput: String): String {
-    val inputDateFormat = SimpleDateFormat("dd/MM/yyyy HH:mm")
-    val outputDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
-    outputDateFormat.timeZone = TimeZone.getTimeZone("MST")
-    val date = inputDateFormat.parse("$dateInput $timeInput")
-    return outputDateFormat.format(date)
-}
-
-fun compareTimes(dateStart: String, startTime: String, dateEnd: String, endTime: String):Boolean{
-    val dateFormat = SimpleDateFormat("dd/MM/yyyy HH:mm")
-    val startDateTime = dateFormat.parse("$dateStart $startTime")
-    val endDateTime = dateFormat.parse("$dateEnd $endTime")
-    return endDateTime.after(startDateTime)
 }
